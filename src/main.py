62e8109f24e17ed29271c62ae752e22ad50fc0c3
Vivaan Malik <malikvivaan@gmail.com>
2025-09-02 02:10:57 +0530

    automated

--- src/main.py
+++ src/main.py
@@ -16,6 +16,8 @@ sender = os.getenv("EMAIL_USER")
 receiver = os.getenv("TEMP_RECIEVER")
 password = os.getenv("EMAIL_PASS")
 unsplashAccessKey = os.getenv("UNSPLASH_ACCESS_KEY")
+imurlthing = os.getenv("IMAGE_URL")
+print(imurlthing)
 
 url = "https://api.unsplash.com/photos/random"
 retryCount = 3
